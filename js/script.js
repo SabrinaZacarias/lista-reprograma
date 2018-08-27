bd9592e84bbee56b788d87981da00cc9912a6430
Sabrina <sabrina.zacs@gmail.com>
2018-08-26 22:12:08 -0300

    adicionar itens de conclusão e exclusão da tarefa, ajustar background e formatação de texto

--- js/script.js
+++ js/script.js
@@ -5,26 +5,45 @@ let list = document.querySelector("#novaTarefa");
 
 
 function adicionar(){
-    if(tarefas.value.trim()){
-        let item = document.createElement("li");
-              
+
+    let item = document.createElement("li");
+    item.setAttribute('class', 'itens')
+    item.classList.add('itens')
+
+        if(tarefas.value.trim()){         
+        
         textnode = document.createTextNode(tarefas.value);
         item.appendChild(textnode)
         
         list.insertBefore(item, list.childNodes[0]);
-
+        
         document.querySelector('#semTarefas').style.display = "none";
         document.querySelector('#comTarefas').style.display = "flex";    
         
+        let concluir = document.createElement('button');
+        concluir.setAttribute('class', 'concluir');
+        concluir.classList.add('concluir');
+        item.appendChild(concluir);
+        
+        
+        let excluir = document.createElement('button');
+        excluir.setAttribute('class', 'excluir');
+        excluir.classList.add('excluir');
+        item.appendChild(excluir);
+
         
     }
 }
 
-document.querySelector('button').onclick = adicionar;
+document.querySelector('#enviar').onclick = adicionar;
 
 tarefas.onkeyup = function(evento){
     if(evento.key === "Enter"){
         adicionar();
     }
 }
+
+ 
+
+
 
