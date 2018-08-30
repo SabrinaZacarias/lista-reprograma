21f84271eac4478de001b1a581e18c55d54d23e3
Sabrina <sabrina.zacs@gmail.com>
2018-08-30 10:34:21 -0300

    alterar layout e construir função de alteração do texto ao cloncluir tarefa

--- js/script.js
+++ js/script.js
@@ -5,33 +5,49 @@ let list = document.querySelector("#novaTarefa");
 
 
 function adicionar(){
-
+    
     let item = document.createElement("li");
     item.setAttribute('class', 'itens')
     item.classList.add('itens')
-
-        if(tarefas.value.trim()){         
+    
+    if(tarefas.value.trim()){         
         
         textnode = document.createTextNode(tarefas.value);
         item.appendChild(textnode)
         
         list.insertBefore(item, list.childNodes[0]);
-        
+        tarefas.value= "";
         document.querySelector('#semTarefas').style.display = "none";
         document.querySelector('#comTarefas').style.display = "flex";    
         
         let concluir = document.createElement('button');
         concluir.setAttribute('class', 'concluir');
         concluir.classList.add('concluir');
+        concluir.setAttribute('title', 'Concluir tarefa');
         item.appendChild(concluir);
         
         
         let excluir = document.createElement('button');
         excluir.setAttribute('class', 'excluir');
         excluir.classList.add('excluir');
+        excluir.setAttribute('title', 'Excluir tarefa');
         item.appendChild(excluir);
-
         
+        excluir.addEventListener('click', () => {
+            item.parentNode.removeChild(item);
+                        
+        })
+        concluir.addEventListener('click', () => {
+            if (!item.classList.contains("concluida")) {
+                item.classList.add('concluida');
+                
+            }
+            else{
+                item.classList.remove('concluida');
+                
+            }
+            
+        })
     }
 }
 
@@ -43,7 +59,7 @@ tarefas.onkeyup = function(evento){
     }
 }
 
- 
+
 
 
 
